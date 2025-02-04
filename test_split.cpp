a04be62cf294490103d65498bfe70267db3a605f
Arya Verma <aryaverm@usc.edu>
2025-02-04 05:54:03 +0000

    all done with part1

--- test_split.cpp
+++ test_split.cpp
@@ -8,10 +8,35 @@ Create linked lists and split them with your split() function.
 You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
-
+using namespace std;
 #include "split.h"
+#include <iostream>
+
 
 int main(int argc, char* argv[])
 {
+  Node* in = nullptr;
+  for (int i = 6; i>= 1; i--){
+    in = new Node(i, in);
+  }
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+  split(in, odds, evens);
+
+  Node* printOdds = odds;
+  cout << "odds:";
+  while(printOdds){
+    cout << printOdds->value << " ";
+    printOdds = printOdds->next;
+  }
+  cout << endl;
+
+  Node* printEvens = evens;
+  cout << "evens:";
+  while(printEvens){
+    cout << printEvens->value << " ";
+    printEvens = printEvens->next;
+  }
+  cout << endl;
 
 }
